59dee746050b61dcc15541a5009ca5d12b33531d
lavoiem <lavoiem@educ.college-montreal.qc.ca>
2023-02-15 10:41:19 -0500

    Start

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ SCREEN_HEIGHT = 480
 WINDOW_TITLE = "Je suis Arcane"
 
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
-
 arcade.set_background_color(arcade.color.SKY_BLUE)
-
 arcade.start_render()
 
-arcade.finish_render()
 
+arcade.draw_lrtb_rectangle_filled(
+    0,
+    SCREEN_WIDTH, SCREEN_HEIGHT / 4,
+    0,
+    arcade.csscolor.DARK_GOLDENROD)
+
+arcade.finish_render()
 arcade.run()
+
 
