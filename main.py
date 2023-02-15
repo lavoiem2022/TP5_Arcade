fcdaa272970e09af451c85d9fb2d10e526af17bd
lavoiem <lavoiem@educ.college-montreal.qc.ca>
2023-02-15 10:26:42 -0500

    Start

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import random
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
+WINDOW_TITLE = "Je suis Arcane"
 
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
 
-def main():
-    pass
+arcade.set_background_color(arcade.color.SKY_BLUE)
+
+arcade.start_render()
+
+arcade.finish_render()
+
+arcade.run()
+
