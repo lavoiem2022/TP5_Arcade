63a2f37ee070b1b514fa430bf3dcb6bda575a7ed
lavoiem <lavoiem@educ.college-montreal.qc.ca>
2023-05-04 09:12:46 -0400

    Start...

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ def draw_sun(x, y):
     arcade.draw_line(x, y, x - 100, y + 100, arcade.color.AMBER, 3)
     arcade.draw_line(x, y, x - 100, y - 100, arcade.color.AMBER, 3)
 
+def text(x, y):
+    arcade.draw_text("Mehdi Serge Insane Project !!!", 700, IMAGE_HEIGHT - 500, arcade.color.PURPLE_HEART)
 
 def draw_cloud(x, y):
     #Cloud
@@ -38,6 +40,17 @@ def draw_cloud(x, y):
     arcade.draw_ellipse_filled(x + 50, y, x / 20 * 3, 35, arcade.csscolor.WHITE)
     arcade.draw_ellipse_filled(x + 150, y, x / 20 * 3, 35, arcade.csscolor.WHITE)
 
+def draw_grass(x, y):
+    points = [(928, 170.7), (928, 256), (896, 256), (896, 298.7), (864, 298.7), (851.2, 213.3), (812.8, 213.3), (800, 256),
+              (774.4, 256), (761.6, 298.7), (729.6, 256), (704, 256), (691.2, 291.1), (678.4, 256), (659.2, 221.9), (640, 256),
+              (640, 170.7)]
+    arcade.draw_polygon_filled(points, arcade.color.BUD_GREEN)
+    points1 = [(460, 85), (460, 128), (490, 128), (450, 149), (432, 148), (425, 108), (406, 100),
+              (400, 128),
+              (360, 128), (380, 149), (360, 128), (352, 128), (350, 140), (339, 128), (328, 111),
+              (320, 128),
+              (320, 85)]
+    arcade.draw_polygon_filled(points1, arcade.color.BUD_GREEN)
 
 
 def main():
@@ -54,6 +67,11 @@ def main():
     draw_cloud(700, 900)
 
     draw_maison(500, 400)
+
+    draw_grass(500,350)
+
+    text(1024, 600)
+
 
 
     arcade.finish_render()
